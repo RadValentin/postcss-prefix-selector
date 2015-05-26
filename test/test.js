0ea6367011435af03a1e14bde85a56b2926ddacd
Nicolas Dupont <npg.dupont@gmail.com>
2015-05-26 19:25:49 +0200

    Add `exclude` option to avoid prefixing some selectors

--- test/test.js
+++ test/test.js
@@ -21,3 +21,16 @@ it('should prefix a group of selectors', function () {
   assert(~out.indexOf('.hello .a'))
   assert(~out.indexOf('.hello .b'))
 })
+
+it('should avoid prefixing excluded selectors', function () {
+  var out = postcss().use(prefix({
+    prefix: '.hello ',
+    exclude: ['body', '.a *:not(.b)']
+  })).process('.a, .b {}\n body {}\n .a *:not(.b) {}\n .c {}').css
+
+  assert(~out.indexOf('.hello .a'))
+  assert(~out.indexOf('.hello .b'))
+  assert(~out.indexOf('.hello .c'))
+  assert(out.indexOf('.hello body'))
+  assert(out.indexOf('.hello .a *:not(.b)'))
+})
