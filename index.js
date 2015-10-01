5ef3ab05274dfe14f12ddf39b25ffc0332a5a520
Valentin Radulescu <radulescu.valentin@gmail.com>
2015-10-01 21:25:26 +0300

    Parse selectors array instead of splitting #3

--- index.js
+++ index.js
@@ -7,15 +7,13 @@ module.exports = function (options) {
   assert(prefix)
   if (!/\s+$/.test(prefix)) prefix += ' '
   return function (root) {
-    root.eachRule(function (rule) {
-      // pretty sure this splitting breaks for certain selectors
-      var selectors = rule.selector.split(/\s*,\s*/g)
-      rule.selector = selectors.map(function (selector) {
+    root.walkRules(function (rule) {
+      rule.selectors = rule.selectors.map(function (selector) {
         if (options.exclude && ~options.exclude.indexOf(selector)) {
           return selector;
         }
         return prefix + selector
-      }).join(', ')
+      })
     })
   }
 }
