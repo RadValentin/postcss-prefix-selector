053ba2c16a5ab20b18d348e47e959d19f02c1b01
Valentin Radulescu <valentin.radulescu@hootsuite.com>
2016-03-24 16:27:10 +0200

    Add support for regex in exclude rules #5

--- index.js
+++ index.js
@@ -13,11 +13,21 @@ module.exports = function (options) {
       }
 
       rule.selectors = rule.selectors.map(function (selector) {
-        if (options.exclude && ~options.exclude.indexOf(selector)) {
+        if (options.exclude && excludeSelector(selector, options.exclude)) {
           return selector
         }
         return prefix + selector
       })
     })
   }
+}
+
+function excludeSelector(selector, excludeArr) {
+  return excludeArr.some(function(excludeRule) {
+    if (excludeRule instanceof RegExp) {
+      return excludeRule.test(selector)
+    } else {
+      return selector === excludeRule
+    }
+  });
 }
--- test/test.js
+++ test/test.js
@@ -25,14 +25,16 @@ it('should prefix a group of selectors', function () {
 it('should avoid prefixing excluded selectors', function () {
   var out = postcss().use(prefix({
     prefix: '.hello ',
-    exclude: ['body', '.a *:not(.b)']
-  })).process('.a, .b {}\n body {}\n .a *:not(.b) {}\n .c {}').css
+    exclude: ['body', '.a *:not(.b)', /class-/]
+  })).process('.a, .b {}\n body {}\n .a *:not(.b) {}\n .c {}\n .class-a{}\n .class-b{}').css
 
   assert(~out.indexOf('.hello .a'))
   assert(~out.indexOf('.hello .b'))
   assert(~out.indexOf('.hello .c'))
   assert(!~out.indexOf('.hello body'))
   assert(!~out.indexOf('.hello .a *:not(.b)'))
+  assert(!~out.indexOf('.hello .class-a'))
+  assert(!~out.indexOf('.hello .class-b'))
 })
 
 it('should skip @keyframes selectors', function () {
